5c9f40e22cc2e3968bd1df843cf8e7102d73890e
tam le van <tamle.dev@gmail.com>
2023-11-15 22:51:17 +0700

    Study C# 12

---
+++ CollectionExpressions/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+Console.WriteLine("Collection Expressions");
+
+Console.WriteLine("Few of the ways collections might be created:");
+
+int[] arrayOne = new int[] { 1, 2, 3, 4 };
+int[] emptyArrayTwo = Array.Empty<int>();
+List<int> listOne = new List<int> { 1, 2, 3, 4 };
+
+Console.WriteLine("Ended...");
